26022d476a5b3badced4dd2706b432b6a2a0a1c5
Ding Pang <dpang@middlebury.edu>
2019-06-20 17:51:54 -0400

    kd

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {Page} = require('../models')
-const {addPage} = require('../views')
+const {Page} = require('../models');
+const {addPage} = require('../views');
+const {wikiPage} = require ('../views');
 
 router.get('/', (req, res, next) => {
 
@@ -14,10 +15,10 @@ router.post('/', async (req, res, next) => {
         content: req.body.content
     });
 
-    console.log(page);
     try {
         await page.save();
-        res.redirect('/');
+        console.log(page);
+        res.redirect(`/wiki/${page.slug}`);
     } catch (error) {
         next(error)
     }
@@ -26,5 +27,15 @@ router.post('/', async (req, res, next) => {
 router.get('/add', (req, res, next) => {
     res.send(addPage());
 });
+
+router.get('/:slug', async (req, res, next) => {
+    try{
+        const targetPage = await Page.findOne({where: {slug: `${req.params.slug}`}})
+        res.send(wikiPage(targetPage));}
+    catch (error) {
+        next(error)
+    }
+  });  
+
 
 module.exports= router;
